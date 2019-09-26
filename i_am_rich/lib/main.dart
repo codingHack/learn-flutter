186c02949a6287916e8e465ab249eeb9d96e3901
codingHack <dbstjdwo1000@naver.com>
2019-09-26 22:32:35 +0900

    my-card

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp());
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('hello'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Text("hello"),
+      ),
+    ),
+  );
+}
